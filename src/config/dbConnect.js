72d274bd1d255128b7434e4f25acff04a478240b
Enos Gabriel <64032529+enosgb@users.noreply.github.com>
2022-08-08 10:10:01 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect("mongodb+srv://SU:<password>@cluster0.q28klwk.mongodb.net/api-rest");
+mongoose.connect("your mongo connect configuration");
 
 let db = mongoose.connection;
 
